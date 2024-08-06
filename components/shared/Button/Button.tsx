4085b41407273e8a3fbf48aa5f58ec01a945de1f
Francisco Arleo <franciscoarleo336@gmail.com>
2024-08-06 18:02:18 -0500

    refactor: Update button styles for different types and add active state

--- components/shared/Button/Button.tsx
+++ components/shared/Button/Button.tsx
@@ -32,34 +32,35 @@ export const buttonClass = (type: basicTypes) => {
     switch (type) {
         case basicTypes.Primary:
             classText +=
-                'border rounded-md bg-primary-500 text-primary-50 hover:bg-primary-600';
+                'border rounded-md bg-primary-500 text-primary-50 hover:bg-primary-600 active:bg-primary-700';
             break;
         case basicTypes.Secondary:
             classText +=
-                'border rounded-md bg-secondary-500 text-secondary-50 hover:bg-secondary-600';
+                'border rounded-md bg-secondary-500 text-secondary-50 hover:bg-secondary-600 active:bg-secondary-700';
             break;
         case basicTypes.Info:
             classText +=
-                'border rounded-md bg-info-500 text-gray-700 hover:bg-info-600';
+                'border rounded-md bg-info-500 text-gray-700 hover:bg-info-600 active:bg-info-700';
             break;
         case basicTypes.Success:
             classText +=
-                'border rounded-md bg-success-700 text-success-50 hover:bg-success-600';
+                'border rounded-md bg-success-700 text-success-50 hover:bg-success-800 active:bg-success-900';
             break;
         case basicTypes.Warning:
             classText +=
-                'border rounded-md bg-warning-500 text-gray-700 hover:bg-warning-600';
+                'border rounded-md bg-warning-500 text-gray-700 hover:bg-warning-600 active:bg-warning-700';
             break;
         case basicTypes.Danger:
             classText +=
-                'border rounded-md bg-danger-500 text-danger-50 hover:bg-danger-600';
+                'border rounded-md bg-danger-500 text-danger-50 hover:bg-danger-600 active:bg-danger-700';
             break;
         case basicTypes.Text:
-            classText += 'bg-transparent text-primary-50 hover:bg-primary-50';
+            classText +=
+                'bg-transparent text-gray-50 hover:text-gray-300 active:text-gray-400';
             break;
         case basicTypes.Accent:
             classText +=
-                'border rounded-md bg-accent-500 text-accent-50 hover:bg-accent-600';
+                'border rounded-md bg-accent-500 text-accent-50 hover:bg-accent-600 active:bg-accent-700';
             break;
         default:
             break;
