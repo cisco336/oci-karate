c3f76ebaacb34adece87f71d24a3d34b9dae2a32
Francisco Arleo <franciscoarleo336@gmail.com>
2024-08-07 08:18:09 -0500

    refactor: Update button styles for different types and add active state

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,19 +1,22 @@
+'use client';
 import { Button, basicTypes } from '@/components/shared/Button';
 import React from 'react';
 
-const page = async () => {
+const buttons = () => {
     const buttons = Object.values(basicTypes);
-    return (
-        <div className="flex gap-4 p-4">
-            {buttons.map((button) => (
-                <Button
-                    key={button}
-                    type={button as basicTypes}>
-                    {button}
-                </Button>
-            ))}
-        </div>
-    );
+
+    return buttons.map((button) => (
+        <Button
+            key={button}
+            type={button as basicTypes}
+            click={() => console.log(`Button ${button} clicked`)}>
+            {button}
+        </Button>
+    ));
+};
+
+const page = async () => {
+    return <div className="flex gap-4 p-4">{buttons()}</div>;
 };
 
 export default page;
--- components/shared/Button/Button.tsx
+++ components/shared/Button/Button.tsx
@@ -1,14 +1,14 @@
 import React, { PropsWithChildren } from 'react';
 
 export enum basicTypes {
-    Primary = 'Primary',
-    Secondary = 'Secondary',
-    Info = 'Info',
-    Success = 'Success',
-    Warning = 'Warning',
-    Danger = 'Danger',
-    Text = 'Text',
-    Accent = 'Accent',
+    Primary = 'primary',
+    Secondary = 'secondary',
+    Info = 'info',
+    Success = 'success',
+    Warning = 'warning',
+    Danger = 'danger',
+    Text = 'text',
+    Accent = 'accent',
 }
 
 export enum buttonTypes {
@@ -24,46 +24,23 @@ export interface iButton {
     icon?: string;
     type?: basicTypes;
     buttonType?: buttonTypes;
-    onClick?: () => any;
+    click?: () => any;
 }
 
 export const buttonClass = (type: basicTypes) => {
     let classText = 'relative px-3 py-2 ';
-    switch (type) {
-        case basicTypes.Primary:
-            classText +=
-                'border rounded-md bg-primary-500 text-primary-50 hover:bg-primary-600 active:bg-primary-700';
-            break;
-        case basicTypes.Secondary:
-            classText +=
-                'border rounded-md bg-secondary-500 text-secondary-50 hover:bg-secondary-600 active:bg-secondary-700';
-            break;
-        case basicTypes.Info:
-            classText +=
-                'border rounded-md bg-info-500 text-gray-700 hover:bg-info-600 active:bg-info-700';
-            break;
-        case basicTypes.Success:
-            classText +=
-                'border rounded-md bg-success-700 text-success-50 hover:bg-success-800 active:bg-success-900';
-            break;
-        case basicTypes.Warning:
-            classText +=
-                'border rounded-md bg-warning-500 text-gray-700 hover:bg-warning-600 active:bg-warning-700';
-            break;
-        case basicTypes.Danger:
-            classText +=
-                'border rounded-md bg-danger-500 text-danger-50 hover:bg-danger-600 active:bg-danger-700';
-            break;
-        case basicTypes.Text:
-            classText +=
-                'bg-transparent text-gray-50 hover:text-gray-300 active:text-gray-400';
-            break;
-        case basicTypes.Accent:
-            classText +=
-                'border rounded-md bg-accent-500 text-accent-50 hover:bg-accent-600 active:bg-accent-700';
-            break;
-        default:
-            break;
+    const textColor =
+        type === basicTypes.Warning || type === basicTypes.Info
+            ? 'text-gray-900'
+            : 'text-gray-50';
+    const bgColor = type === basicTypes.Success ? 700 : 500;
+
+    if (type === basicTypes.Text) {
+        classText += `bg-transparent text-gray-50 hover:text-gray-300 active:text-gray-400`;
+    } else {
+        classText += `border rounded-md bg-${type}-${bgColor} ${textColor} hover:bg-${type}-${
+            bgColor + 100
+        } active:bg-${type}-${bgColor + 200}`;
     }
 
     return classText;
@@ -74,7 +51,7 @@ export const Button = ({
     icon,
     loading,
     disabled,
-    onClick,
+    click,
     type = basicTypes.Primary,
     buttonType = buttonTypes.Button,
     children,
@@ -91,7 +68,7 @@ export const Button = ({
             className={`${buttonClass(type)}`}
             type={buttonType}
             disabled={disabled || loading}
-            onClick={onClick}>
+            onClick={click}>
             {loading ? loadingIndicator : null}
             {icon ? icon : null}
             {children || label}
