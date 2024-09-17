dc139dc0f7f7b4189a0c90273a1c775d63d94dc5
Francisco Arleo <franciscoarleo336@gmail.com>
2024-09-16 20:08:50 -0500

    Toggle first commit

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { ButtonsExample } from '@/components/shared/Button';
 import { InputExample } from '@/components/shared/Input';
+import { ToggleExample } from '@/components/shared/Toggle';
 import React from 'react';
 
 const page = () => {
@@ -8,6 +9,7 @@ const page = () => {
     <div className="flex flex-col gap-14 p-4 flex-wrap max-w-[1200px]">
       <ButtonsExample />
       <InputExample />
+      <ToggleExample />
     </div>
   );
 };
